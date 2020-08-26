1365e019f3c23f0c91fc94f61de58024c8d758c8
MicheleMorelli <morelli.michele@hotmail.it>
2020-08-26 10:45:22 +0100

    dummy receive method

--- src/main/scala/ActorTest.scala
+++ src/main/scala/ActorTest.scala
@@ -2,4 +2,13 @@ import akka.actor.Actor
 
 case class ActorTest() extends Actor {
 
+  private def pr[A](obj:A, f: A=> A):Unit = {
+    println(s"I received a ${obj.getClass.getName} instance: $obj.\nApplying ${f.toString}: ${f(obj)}")
+  }
+
+  override def receive: Receive = {
+    case s:String => pr[String](s, _.toUpperCase)
+    case n:Int => pr[Int](n, _ * 3)
+    case other => pr[Any](other, x => println(s"GENERIC ACTION on $other"))
+  }
 }
