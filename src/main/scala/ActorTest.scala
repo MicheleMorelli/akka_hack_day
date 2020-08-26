043a24ad8c7792dfc14c24337a80e269077e4f21
MicheleMorelli <morelli.michele@hotmail.it>
2020-08-26 11:10:49 +0100

    first simple actor is running

--- src/main/scala/ActorTest.scala
+++ src/main/scala/ActorTest.scala
@@ -3,12 +3,13 @@ import akka.actor.Actor
 case class ActorTest() extends Actor {
 
   private def pr[A](obj:A, f: A=> A):Unit = {
-    println(s"I received a ${obj.getClass.getName} instance: $obj.\nApplying ${f.toString}: ${f(obj)}")
+    println(s"I received a ${obj.getClass.getSimpleName} instance: $obj.\nApplying function to it: ${f(obj)}")
   }
 
   override def receive: Receive = {
     case s:String => pr[String](s, _.toUpperCase)
     case n:Int => pr[Int](n, _ * 3)
-    case other => pr[Any](other, x => println(s"GENERIC ACTION on $other"))
+    case n:Double => pr[Double](n, _ / 1.2)
+    case other => pr[Any](other, x => s"GENERIC ACTION on $x")
   }
 }
--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,4 +1,13 @@
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 object Demo extends App {
 
+  val as:ActorSystem = ActorSystem("TestSystem")
+  val actor:ActorRef = as.actorOf(Props[ActorTest], "TestActor")
+
+  actor ! "aaaaaa"
+  actor ! 9879
+  actor ! 'a'
+  actor ! "\\d".r
+  actor ! 2.34
 
 }
