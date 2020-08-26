f0c1813b4a81d4d675d5fe995621ab4034c5eb09
MicheleMorelli <morelli.michele@hotmail.it>
2020-08-26 17:08:43 +0100

    closing

--- src/main/scala/TestDemo.scala
+++ src/main/scala/TestDemo.scala
@@ -13,21 +13,21 @@ import scala.concurrent.duration._
 object TestDemo extends App {
 
   val as: ActorSystem = ActorSystem("TestSystem")
-  val actor: ActorRef = as.actorOf(Props[ActorTest], "TestActor")
-  //
-  //  actor ! "aaaaaa"
-  //  actor ! 9879
-  //  actor ! 'a'
-  //  actor ! "\\d".r
-  //  actor ! 2.34
-  //
+  // =========================================================================
+//  val actor: ActorRef = as.actorOf(Props[ActorTest], "TestActor")
+//    actor ! "aaaaaa"
+//    actor ! 9879
+//    actor ! 'a'
+//    actor ! "\\d".r
+//    actor ! 2.34
+  // ===============================================================================
   //  val wordShortener1:ActorRef = as.actorOf(Props[WordShorternerActor], "WordShorthener1")
   //  val wordShortener2:ActorRef = as.actorOf(Props[WordShorternerActor], "WordShorthener2")
   //
   //  wordShortener1 ! startMessage("This is a test!", wordShortener2)
 
 
-
+  //===========================================================================================================
   val asker: ActorRef = as.actorOf(Props(ActorAsking("Tom", "Smith", 45)), "asker")
   val responder: ActorRef = as.actorOf(Props(ActorAsking("Julie", "London", 39)), "responder")
 
@@ -40,12 +40,11 @@ object TestDemo extends App {
   val ans2: Future[Any] = responder ? AskMessage(Question("surname"))
 
   for {
-    a1 <- ans1
-    a2 <- ans2
-  } yield println(s"I received the following answers: \n$a1\n$a2")
+    _ <- ans1
+    _ <- ans2
+  } yield println(s"I received the following answers: \n$ans1\n$ans2")
 
-
-
+  
   as.terminate
 
 }
